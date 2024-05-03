53b34c8f82e47954833e5b0d4b19448bb464ca56
Derlis Caballero <derliscaballero91@gmail.com>
2024-05-03 14:49:22 -0400

    commit local

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    virtualbox = {
+      source = "terra-farm/virtualbox"
+      version = "0.2.2-alpha.1"
+    }
+  }
+}
+
+# There are currently no configuration options for the provider itself.
+
+resource "virtualbox_vm" "node" {
+  count     = 1
+  name      = format("node-%02d", count.index + 1)
+  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  cpus      = 1
+  memory    = "512 mib"
+  
+
+  network_adapter {
+    type = "nat"
+  }
+}
+
